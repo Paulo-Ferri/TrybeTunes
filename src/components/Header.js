dcefabfa2ccdda43e0675ba8327cf1135c15000d
Paulo Afonso <pauloferrisouza@gmail.com>
2022-02-08 09:53:44 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -28,6 +29,11 @@ class Header extends React.Component {
         {carregando
           ? <Loading />
           : <p data-testid="header-user-name">{`Seja bem-vindo(a), ${name}`}</p>}
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }
