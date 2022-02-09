b32272942bd755df4700ec8f5c87832ae203f687
Paulo Afonso <pauloferrisouza@gmail.com>
2022-02-09 11:46:40 -0300

    requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 const DOIS = 2;
 class Search extends React.Component {
@@ -8,6 +11,9 @@ class Search extends React.Component {
     this.state = {
       search: '',
       buttonDisabled: true,
+      carregando: false,
+      albuns: [],
+      artista: '',
     };
   }
 
@@ -27,8 +33,28 @@ class Search extends React.Component {
     });
   }
 
+  reqAlbuns = async () => {
+    const { search } = this.state;
+    const artista = search;
+    this.setState({
+      carregando: true,
+    });
+    const albunsRequisitados = await searchAlbumsAPI(artista);
+    return this.setState({
+      search: '',
+      albuns: albunsRequisitados,
+      carregando: false,
+      artista,
+    });
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.reqAlbuns();
+  }
+
   render() {
-    const { search, buttonDisabled } = this.state;
+    const { search, buttonDisabled, albuns, carregando, artista } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -46,10 +72,30 @@ class Search extends React.Component {
             data-testid="search-artist-button"
             type="button"
             disabled={ buttonDisabled }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
         </form>
+        { carregando ? <Loading /> : (
+          <p>
+            Resultado de álbuns de:
+            {` ${artista}`}
+          </p>
+        )}
+        {
+          albuns.length > 0
+            ? albuns.map((album) => (
+              <Link
+                to={ `/album/${album.collectionId}` }
+                data-testid={ `link-to-album-${album.collectionId}` }
+                key={ album.collectionId }
+              >
+                { album.collectionName }
+                <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+              </Link>
+            )) : <p>Nenhum álbum foi encontrado</p>
+        }
       </div>
     );
   }
