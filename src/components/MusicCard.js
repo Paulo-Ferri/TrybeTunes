cf9d06d433be7774d57198d4f964c64d0ed21dc8
Paulo Afonso <pauloferrisouza@gmail.com>
2022-02-12 20:53:38 -0300

    requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
-  render() {
-    const { music } = this.props;
-    return (
-      <div>
-        <p>{ music.trackName }</p>
-        <audio data-testid="audio-component" src={ music.previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+    };
   }
+
+   onChecked = async (music) => {
+     this.setState({
+       carregando: true,
+     });
+     await addSong(music);
+     this.setState({
+       carregando: false,
+     });
+   };
+
+   render() {
+     const { music } = this.props;
+     const { carregando } = this.state;
+     return (
+       <div>
+         <p>{ music.trackName }</p>
+         <audio data-testid="audio-component" src={ music.previewUrl } controls>
+           <track kind="captions" />
+           O seu navegador não suporta o elemento
+           {' '}
+           <code>audio</code>
+           .
+         </audio>
+         <label
+           htmlFor="favorito"
+         >
+           <input
+             type="checkbox"
+             data-testid={ `checkbox-music-${music.trackId}` }
+             id="favorito"
+             onChange={ () => this.onChecked(music) }
+           />
+         </label>
+         {carregando && <Loading />}
+       </div>
+     );
+   }
 }
 
 MusicCard.propTypes = {
